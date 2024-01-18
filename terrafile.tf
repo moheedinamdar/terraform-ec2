9ae0ecac9e03ffc3a169b5b306ee8eb377521076
Moheed Inamdar <moheedinamdar@gmail.com>
2024-01-18 16:06:18 +0530

    initial commit

---
+++ terrafile.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">=1.0.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.27.0"
+    }
+  }
+
+  # backend "s3" {
+  #   bucket = "fss-remotestate2"
+  #   key    = "aws/terraform.tfstate"
+  #   region = var.region_project
+  # }
+}
+
+provider "aws" {
+  region = var.region_project
+  profile = "terraform"
+
+  default_tags {
+    tags = {
+      owner      = var.owner_project
+      managed-by = var.managed_by_project
+    }
+  }
+}
+
+module "network" {
+  source = "./modules/network"
+
+  vpc_cidr = var.vpc_cidr_project
+  az_count = var.az_count_project
+
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  subnet        = module.network.subnet_public_id[0]
+  sg            = module.network.sg_public_id
+  instance_type = var.instance_type_project
+}
---
+++ variables.tf
@@ -0,0 +1,43 @@
+# Used in the Terrafile.tf
+
+variable "owner_project" {
+  description = "Owner of this project"
+  type        = string
+  default     = "Moheed Inamdar"
+}
+
+variable "managed_by_project" {
+  description = "How this project is managed or created"
+  type        = string
+  default     = "IAC Terraform"
+}
+
+variable "region_project" {
+  description = "Region used in this project"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
+# Used in the Terrafile.tf to populate the Network module
+
+variable "vpc_cidr_project" {
+  description = "VPC CIDR used in this project"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count_project" {
+  description = "Quantity of Availability Zones used in this project"
+  type        = number
+  default     = 3
+}
+
+
+# Used in the Terrafile.tf to populate the EC2 module
+
+variable "instance_type_project" {
+  description = "Instance type used in this project"
+  type        = string
+  default     = "t3.medium"
+}
